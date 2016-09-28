580fec7c31a871d7fee41a5945479bce1c102cad
thaibm <thaibm.uet@gmail.com>
2016-09-28 22:48:32 +0700

    Header of stock site.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var cssnano = require('gulp-cssnano');
+var livereload = require('gulp-livereload');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+gulp.task('sass', function() {
+    return gulp.src('sass/style.scss')
+        .pipe(sass())
         .pipe(cssnano())
         .pipe(rename('custom-style.css'))
         .pipe(gulp.dest('assets/css'))
+        .pipe(livereload());
 });
 
 //Watch task
 gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('./sass/**/*.scss', ['styles']);
 });
