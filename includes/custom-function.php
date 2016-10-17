02e5fd9da1105d5168efc5ae37ad6d9a399f11bf
daitd <neverfrown.95@gmail.com>
2016-10-17 20:18:34 +0700

    update recent post

--- includes/custom-function.php
+++ includes/custom-function.php
@@ -35,6 +35,6 @@ add_filter('siteorigin_panels_widget_dialog_tabs', 'stock_market_add_widget_tabs
 */
 if ( ! function_exists( 'handmade_entry_post_info' ) ) {
 	function stock_entry_post_info() {
-		echo printf( '<span class="meta1">On</span> <span class="meta2">%1$s</span> <span class="meta1">by</span> <span class="meta2">%2$s</span> ', get_the_date(), get_the_author() );
+		echo printf( '<span class="meta1">On</span> <span class="meta2">%1$s</span>', get_the_date() );
 	}
 }
